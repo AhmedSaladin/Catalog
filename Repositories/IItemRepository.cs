7bb608df9fe7c287f6403f8af8b2c3e23675b227
Ahmed Saladin <ahmed_salahdin@hotmail.com>
2022-07-16 23:13:06 +0200

    Add put route

--- Repositories/IItemRepository.cs
+++ Repositories/IItemRepository.cs
@@ -5,7 +5,11 @@ namespace Catalog.Repositories
   public interface IItemsRepository
   {
     Item GetItem(Guid id);
+
     IEnumerable<Item> GetItems();
+
     void CreateItem(Item item);
+
+    void UpdateItem(Item item);
   }
 }
