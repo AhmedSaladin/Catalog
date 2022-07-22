6026293fa9be8aa55ec4c79c5173d5e84ca55285
Ahmed Saladin <ahmed_salahdin@hotmail.com>
2022-07-22 18:27:12 +0200

    Fix asyncSuffix problem

--- Program.cs
+++ Program.cs
@@ -18,7 +18,10 @@ builder.Services.AddSingleton<IMongoClient>(ServiceProvider =>
 
 builder.Services.AddSingleton<IItemsRepository, MongoDbItemsRepository>();
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+  options.SuppressAsyncSuffixInActionNames = false;
+});
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
