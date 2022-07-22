f91092d1fbfed5b95ff7cfc8a2fd7c843614b4ca
Ahmed Saladin <ahmed_salahdin@hotmail.com>
2022-07-22 15:31:18 +0200

    Setting mongo db connection string

--- Program.cs
+++ Program.cs
@@ -1,9 +1,23 @@
 using Catalog.Repositories;
+using Catalog.Settings;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddSingleton<IItemsRepository,InMemItemsRepository>();
+builder.Services.AddSingleton<IMongoClient>(ServiceProvider =>
+{
+  BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
+  BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
+  var settings = builder.Configuration.GetSection("MongoDatabaseSettings").Get<MongoDbSettings>();
+  return new MongoClient(settings.connection);
+});
+
+builder.Services.AddSingleton<IItemsRepository, MongoDbItemsRepository>();
+
 builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -15,8 +29,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
---
+++ settings/MongoDbSettings.cs
@@ -0,0 +1,7 @@
+namespace Catalog.Settings
+{
+  public class MongoDbSettings
+  {
+    public string? connection { get; set; }
+  }
+}
